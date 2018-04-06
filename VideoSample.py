18a34324d9af40b13b4294d2dea22886ac58696d
unknown <1340230090@qq.com>
2018-04-06 09:17:50 +0800

    scenedetect modify

--- VideoSample.py
+++ VideoSample.py
@@ -1,41 +1,68 @@
 # coding:utf-8
 ###### Sat Mar 31 09:38:34 CST 2018
-import Scene.scenedetect
-import Scene.scenedetect.detectors
-import Scene.scenedetect.manager
+from __future__ import print_function
 
-class VideoSample():
+import scenedetect as scenedetect
+import scenedetect.detectors as detectors
+import scenedetect.manager as manager
+
+import configparser
+
+class VideoSample:
     """
         视频采样类
-        V1.0###### Sat Mar 31 09:46:39 CST 2018
     """
     
-    def __init__(self, threshold = 30, 
-        frame_skip = 0, 
+    def __init__(self,
+        useconfig        = False, 
+        threshold        = 30, 
+        frame_skip       = 0, 
         downscale_factor = 50, 
-        save_images = True):
+        save_images      = True):
         """初始化
         
         Keyword Arguments:
+            useconfig {bool} -- 是否读配置文件
             threshold {int} -- 阈值 (default: {30})
             frame_skip {int} -- 跳帧 (default: {0})
             downscale_factor {int} -- 压缩率 (default: {50})
             save_images {bool} -- 是否存储采样图片 (default: {True})
         """
-        content_detector = scenedetect.detectors.ContentDetector(threshold = threshold)
-        self.smgr = scenedetect.manager.SceneManager(detector = content_detector, 
-        frame_skip = frame_skip, 
-        downscale_factor = downscale_factor, 
-        save_images = save_images)
+        self.content_detector = detectors.ContentDetector(threshold = threshold)        
+        if useconfig:
+            conf                  = configparser.ConfigParser()
+            conf.read("Config/config.ini")
+            self.threshold        = conf.getint('sample', 'threshold')
+            self.frame_skip       = conf.getint('sample', 'frame_skip')
+            self.downscale_factor = conf.getint('sample', 'downscale_factor')
+            self.save_images      = conf.getboolean('sample', 'save_images')
+        else:
+            self.threshold        = threshold
+            self.frame_skip       = frame_skip
+            self.downscale_factor = downscale_factor
+            self.save_images      = save_images
 
     def sample(self, videoname):
-        content_detector = scenedetect.detectors.ContentDetector(threshold = 30)
-        smgr = scenedetect.manager.SceneManager(detector =content_detector, 
-        frame_skip = 2, downscale_factor = 30, 
-        save_images = False, save_csv_filename="2017.csv", save_image_prefix='jj', perf_update_rate=1)
-        scenedetect.detect_scenes_file("Videos/20170701_c.mp4", smgr)
-        # time_end = time.time()
-        # print('SUM time = ' + str(time_end - time_start))
-        print("Detected %d scenes in video" % (len(smgr.scene_list)))
-        scenedetect.detect_scenes_file(videoname, self.smgr)
-        print("Detected %d scenes in video (algorithm = content, threshold = 20)." % (len(smgr.scene_list)))
+        """对视频进行取样并返回场景信息
+        
+        Arguments:
+            videoname {string} -- 视频文件名
+        """
+        smgr = manager.SceneManager(
+        detector          = self.content_detector, 
+        frame_skip        = self.frame_skip, 
+        downscale_factor  = self.downscale_factor, 
+        save_images       = self.save_images, 
+        save_csv_filename = "tmp.csv", 
+        save_image_prefix = 'tmp', 
+        perf_update_rate  = 1)
+        scenedetect.detect_scenes_file(videoname, smgr)
+        return smgr.scene_list
+
+
+if __name__ == "__main__":
+    vname     = "Data/Videos/demo.mp4"
+    vsample   = VideoSample(useconfig = True)
+    scenelist = vsample.sample(vname)
+    print("Detected %d scenes in video" % (len(scenelist)))
+    print(scenelist)
