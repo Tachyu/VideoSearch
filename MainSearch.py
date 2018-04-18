cc192f9423911bc318d1563ebf63515d805af04b
unknown <1340230090@qq.com>
2018-04-18 14:37:16 +0800

    MainSearch返回JSON

--- MainSearch.py
+++ MainSearch.py
@@ -5,6 +5,7 @@ from BasicPart import BasicPart
 from FaceRecog import FaceRecog
 from ObjectDet import ObjectDet
 from DBHandler import DBHandler
+import os,json
 
 try:
     from PIL import Image, ImageDraw
@@ -14,15 +15,21 @@ except ImportError:
 
 
 class MainSearch(BasicPart):
-    def __init__(self, prefix, imagename, logfile=None, isShow=False):
+    def __init__(self, prefix, imagename,max_len=10, logfile=None, isShow=False):
         BasicPart.__init__(self, logfile, isShow)
         self.imagename     = imagename
         self.facerecog     = FaceRecog(logfile, isShow)
         self.objectdetect  = ObjectDet(logfile, isShow, single_pic_process=True)
         self.dbhandler     = DBHandler()
         self.SearchFeature = SearchFeature(logfile, isShow)
+        self.max_len = max_len
         self.prefix = prefix
-        
+
+    def read_config(self):
+        # TODO:配置文件
+        self.thumb_prefix = '/var/www/html/thumbs/'  
+        self.thumb_size = '400x300'   
+         
     
     def __get_db_info(self, id_type, id_list):
         """根据id查询数据库
@@ -97,6 +104,56 @@ class MainSearch(BasicPart):
             image_names.append(image_name)
         
         return image_names
+    
+    def mkthumb(self, timestamp, full_videoname, image_name):
+        cmd = '''
+            ffmpeg -ss %d -i %s -y -f image2 -vframes 1 -s %s %s
+            '''%(timestamp,full_videoname,self.thumb_size,image_name)
+        a = subprocess.getoutput(cmd)
+
+    def to_json(self, result_list):
+        json_list = []
+        # 1. 转换文件名到路径，2.同时生成略缩图：400*300
+        for index, re in enumerate(result_list):
+            cpdic = re.dic
+            videopath = cpdic['videoname']
+            video_name = videopath.split('/')[-1]
+            cpdic['videoname'] = video_name
+            cpdic['videopath'] = videopath
+
+            # 截图
+            thumb_name = cpdic['video_name'] +"_s_"+str(cpdic['sceneid']) +".jpg"
+            thumb_name = os.path.join(self.thumb_prefix,thumb_name)
+            if os.path.exists(thumb_name):
+                pass
+            else:
+                st = cpdic['starttime']
+                pt = cpdic['videopath']
+                self.mkthumb(st, pt, thumb_name)
+            cpdic['thumb'] = thumb_name
+            json_list.append(cpdic)
+        return json_list
+
+    def get_search_result_JSON(self):
+        """返回json格式的检索
+        TODO: 完成物体搜索结果
+        """
+        face_idlist = self.search_face()[:self.max_len]
+        face_results = self.get_face_to_video_sceneinfo(face_idlist)
+
+        cont_idlist = self.search_pic()[:self.max_len]
+        cont_results = self.get_content_to_video_sceneinfo(cont_idlist)
+
+        result_json = {}
+        result_json['face_scene_num'] = len(face_idlist)  
+        result_json['face_list'] = self.to_json(face_results)
+
+        result_json['content_scene_num'] = len(cont_idlist)  
+        result_json['content_scene_list'] = self.to_json(cont_results)
+        
+        return json.dumps(result_json)
+
+
 
 class SceneInfo:
     def __init__(self, dic):
@@ -117,18 +174,18 @@ class SceneInfo:
         return hash(self.dic['sceneid'])
 
 if __name__ == '__main__':  
-    ms = MainSearch(prefix='test', imagename="Data/Tmp/tmp.png",isShow=True)
+    ms = MainSearch(prefix='test', max_len = 10, imagename="Data/Tmp/tmp.png",isShow=True)
     # ms = MainSearch(prefix='test', imagename="Data/Tmp/tmp.png",isShow=True)
-    
-    # ms.create_indexs(True)
-    idlist = ms.search_face()[:10]
-    results = ms.get_face_to_video_sceneinfo(idlist)  
-    ms.show_pics(results)
-
-    # idlist = ms.search_pic()[:5]
-    # results = ms.get_content_to_video_sceneinfo(idlist)  
+    print(ms.get_search_result_JSON())
+    # # ms.create_indexs(True)
+    # idlist = ms.search_face()[:10]
+    # results = ms.get_face_to_video_sceneinfo(idlist)  
     # ms.show_pics(results)
-    for re in results:
-        print(re)     
+
+    # # idlist = ms.search_pic()[:5]
+    # # results = ms.get_content_to_video_sceneinfo(idlist)  
+    # # ms.show_pics(results)
+    # for re in results:
+    #     print(re)     
         
         
